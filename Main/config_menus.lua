edb99737619c6dc21f03795995ce8a9f4ae76f0c
DragonSCRIPTS <cleyltondragonscp@gmail.com>
2025-04-20 15:01:59 -0300

    Create config_menus.lua

---
+++ Main/config_menus.lua
@@ -0,0 +1,35 @@
+-- Configuração dos Menus (config_menus.lua)
+-- Este arquivo contém a configuração dos menus disponíveis
+
+return {
+    {
+        nome = "Principal",
+        ativo = true,
+        imagemURL = nil,  -- [off] já que não tem imagem
+    },
+    {
+        nome = "Jogador",
+        ativo = true,
+        imagemURL = "rbxassetid://123456789",  -- [on] com link para imagem
+    },
+    {
+        nome = "Mundo",
+        ativo = true,
+        imagemURL = nil,  -- [off] já que não tem imagem
+    },
+    {
+        nome = "Visual",
+        ativo = true,
+        imagemURL = "rbxassetid://987654321",  -- [on] com link para imagem
+    },
+    {
+        nome = "Combate",
+        ativo = false,  -- Este menu não estará disponível
+        imagemURL = nil,
+    },
+    {
+        nome = "Sobre",
+        ativo = true,
+        imagemURL = nil,  -- [off] já que não tem imagem
+    }
+}
